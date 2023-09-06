b4cbb1c0922dae4f9097d03158b4c0e11f6d0e82
Abolfazl Taj <103893533+Abolfazl-Taj@users.noreply.github.com>
2023-09-06 10:05:27 +0330

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  ////////////////// Acardion Menu
+  let btn = document.querySelector(".open");
+  let openmenu = document.querySelector(".open-menu");
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("rotate");
+    if (openmenu.style.maxHeight) {
+      openmenu.style.maxHeight = null;
+      openmenu.style.border = "0px";
+    } else {
+      openmenu.style.maxHeight = openmenu.scrollHeight + "px";
+      openmenu.style.border = "1px solid #535353";
+    }
+  });
+  ////////////////// Acardion Menu
+
+  /////////////////////////// clock
+  setInterval(timer, 1000);
+  function timer() {
+    let time = document.querySelector(".clock");
+    let now = moment().format("YY/MM/DD dd  , HH:MM:SS");
+    time.innerHTML = `${now}`
+  }
+  /////////////////////////// clock
+});
