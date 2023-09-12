bdcf7a212df4d02408732a7fd5ddeb77a05be0fb
Abolfazl Taj <103893533+Abolfazl-Taj@users.noreply.github.com>
2023-09-12 21:20:51 +0330

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -13,40 +13,67 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
   ////////////////// Acardion Menu
-
+  
   /////////////////////////// clock
   setInterval(timer, 1000);
   function timer() {
     let time = document.querySelector(".clock");
     let now = moment().format("YY/MM/DD  dd , HH:mm:ss");
-    time.innerHTML = now
+    time.innerHTML = now;
   }
   /////////////////////////// clock
-////////////////////////////////////////////Subscribe////////////////////////////////////////////
-// Can also be included with a regular script tag
-var typed = new Typed('.Sub-header', {
-  strings: ["Subscribe Now", "50% Off For Your First Time" , "Only Today"],
-  typeSpeed: 30,
-  backSpeed: 20,
-  loop: true,
-  fadeOut: true,
-  showCursor: false,
-});
-////////////////////////////////////////////Subscribe////////////////////////////////////////////
+  ////////////////////////////////////////////Subscribe////////////////////////////////////////////
+  // Can also be included with a regular script tag
+  var typed = new Typed(".Sub-header", {
+    strings: ["Subscribe Now", "50% Off For Your First Time", "Only Today"],
+    typeSpeed: 30,
+    backSpeed: 20,
+    loop: true,
+    fadeOut: true,
+    showCursor: false,
+  });
+  ////////////////////////////////////////////Subscribe////////////////////////////////////////////
 
-////////////////////////////////////////////INPUT////////////////////////////////////////////
+  ////////////////////////////////////////////INPUT////////////////////////////////////////////
 
-  var typed2 = new Typed('.inputes', {
-    strings: ['Wanna Subscribe ?' , 'Write Your Email' ],
+  var typed2 = new Typed(".inputes", {
+    strings: ["Wanna Subscribe ?", "Write Your Email"],
     typeSpeed: 40,
     backSpeed: 80,
-    attr: 'placeholder',
+    attr: "placeholder",
     bindInputFocusEvents: false,
     loop: true,
     fadeOut: true,
   });
-////////////////////////////////////////////INPUT////////////////////////////////////////////
-    
+  ////////////////////////////////////////////INPUT////////////////////////////////////////////
 
+  ////////////////////////////////////////////Up////////////////////////////////////////////
+  let dokme = $(".scro");
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 800) {
+      dokme.addClass("show");
+    } else {
+      dokme.removeClass("show");
+    }
+  });
 
+  dokme.on("click", function (e) {
+    $("html, body").animate({ scrollTop: 0 }, "500");
+    e.preventDefault();
+  });
+  ////////////////////////////////////////////Up////////////////////////////////////////////
+
+  ////////////////////////////////////////////Darkmode////////////////////////////////////////////
+  let Changer = document.querySelector(".Mode");
+  let light = document.querySelector(".fa-sun");
+  let dark = document.querySelector(".fa-moon");
+  Changer.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark");
+    Changer.classList.toggle("sun");
+    Changer.classList.toggle("moon");
+    light.classList.toggle("off");
+    dark.classList.toggle("off");
+  });
+
+  ////////////////////////////////////////////Darkmode////////////////////////////////////////////
 });
